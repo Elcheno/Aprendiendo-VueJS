ca90409cdca0b7f9d3c72337ecf7c1f152885cdc
Ruben <r.c.a2312@gmail.com>
2024-04-01 15:37:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import './index.css';
+import { Button } from '@/components/ui/button';
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <header>
+
+    <Button>SHADCN</Button>
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <!--<RouterView />-->
+</template>
